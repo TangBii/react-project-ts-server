9e5a055a1de583ded391b1fc59ee0012baed9fd8
TangBii <601098499@qq.com>
2020-03-17 14:23:10 +0800

    信息完善路由

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,8 @@ router.post('/login', (req, res) => {
     if (user) {
       if (autoLogin) {
         res.cookie('userid', user._id, {maxAge})
+      } else {
+        res.cookie('userid', user._id, {maxAge: 1000 * 60 * 10})
       }
       res.send({status: 1, data: user})
     } else {
@@ -46,8 +48,6 @@ router.post('/register', (req, res) => {
           res.send({status: 0, message: '发生未知错误2'})
         }
         
-        
-        
         // 和登陆过程一样，生成一个 Cookie
         res.cookie('userid', user._id, {maxAge})
 
@@ -59,6 +59,26 @@ router.post('/register', (req, res) => {
   })
 })
 
+
+// 信息完善路由
+router.post('/update', (req, res) => {
+  const userid = req.cookies.userid
+  if (!userid) {
+    return res.send({status: 0, message: '用户不存在,请重新登陆'})
+  }
+  UserModel.findByIdAndUpdate({_id: userid}, req.body, (error, oldUser) => {
+    if (error) {
+      return res.send({status: 0, message: '发生未知错误3'})
+    }
+    if (!oldUser) {
+      res.clearCookie('userid')
+      return res.send({status: 0, message: '用户信息无效, 请重新登陆'})
+    }
+    const {_id, username, type} = oldUser
+    const user = Object.assign({_id, username, type}, req.body)
+    res.send({status: 1, data: user})
+  })
+})
 
 
 
